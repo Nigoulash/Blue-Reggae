20b65f350d5dd34ff6dbe778230c762d919bc2b7
Nigoulash <g.n.oneill@outlook.com>
2025-01-13 14:26:46 +0100

    playerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 1.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,39 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        BasicMovement();
         
+    }
+
+
+    void BasicMovement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            {
+                transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            {
+                transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            {
+                transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            {
+                transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+            }
+        }
     }
 }
