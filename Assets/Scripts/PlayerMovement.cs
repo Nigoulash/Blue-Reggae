30ff657907f41856c351ee3b051e673542fdd6b5
Nigoulash <g.n.oneill@outlook.com>
2025-01-15 13:17:27 +0100

    player prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,28 +1,62 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float _origMoveSpeed = 5.0f;
     float realMoveSpeed;
     float halfMoveSpeed;
+
     [SerializeField] float _origJumpSpeed = 0.5f;
     float realJumpSpeed;
     float halfJumpSpeed;
     [SerializeField] int _jumpStrength = 2;
     bool isGrounded = false;
-    [SerializeField] GameObject _groundChecker;
+   
+    [SerializeField] Transform _groundChecker;
+    [SerializeField] LayerMask groundLayer;
+
+    [SerializeField] Transform _leftWallChecker;
+    [SerializeField] Transform _rightWallChecker;
+    [SerializeField] LayerMask wallLayer;
+
+    bool isOnRightWall = false;
+    bool isOnLeftWall = false;
+
+    Rigidbody2D rb;
+
+    CapsuleCollider2D capColl;
+    CircleCollider2D cirColl;
+    
+    float coyoteTime = 0.2f;
+    float coyoteTimeCounter;
+
+    float jumpBufferTime = 0.2f;
+    float jumpBufferCounter;
+
+    Vector2 startPosition;
+
+    //bool canMove;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        capColl = GetComponent<CapsuleCollider2D>();
+        cirColl = GetComponent<CircleCollider2D>();
+        startPosition = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCapsule(_groundChecker.position, new Vector2(1f, 0.2f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+
+        isOnRightWall = Physics2D.OverlapCapsule(_rightWallChecker.position, new Vector2(1f, 0.2f), CapsuleDirection2D.Vertical, 0, wallLayer);
+        isOnLeftWall = Physics2D.OverlapCapsule(_leftWallChecker.position, new Vector2(1f, 0.2f), CapsuleDirection2D.Vertical, 0, wallLayer);
+
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             halfMoveSpeed = _origMoveSpeed / 2;
@@ -38,9 +72,15 @@ public class PlayerMovement : MonoBehaviour
             realJumpSpeed = _origJumpSpeed;
         }
 
+        //if (canMove)
+        
         BasicMovement();
 
         Jump();
+
+        Slide();
+
+        OutOfBounds();
         
     }
 
@@ -65,16 +105,86 @@ public class PlayerMovement : MonoBehaviour
 
     void Jump()
     {
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if (Physics2D.OverlapCircle(_groundChecker.transform.position, 0.1f, 3))
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+
+        if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
+        {
+
             {
-                {
-                    float yPos = transform.position.y + _jumpStrength;
-                    Vector2 targetPosition = new Vector2(transform.position.x, yPos);
-                    transform.position = Vector2.Lerp(transform.position, targetPosition, realJumpSpeed);
-                }
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, _jumpStrength);
+
+                jumpBufferCounter = 0f;
+                    
             }
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow) && rb.linearVelocityX > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, rb.linearVelocityY * 0.5f);
+
+            coyoteTimeCounter = 0f;
         }
     }
+
+    void WallJump()
+    {
+        if (isOnLeftWall && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.linearVelocity = new Vector2(-_jumpStrength / 2, _jumpStrength / 2);
+        }
+
+        if (isOnRightWall && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.linearVelocity = new Vector2(_jumpStrength / 2, _jumpStrength / 2);
+        }
+    }
+
+    void OutOfBounds()
+        {
+            if (transform.position.y < -5f)
+            {
+                transform.position = startPosition;
+            }
+        }
+
+    void Slide()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            capColl.enabled = false;
+            cirColl.enabled = true;
+        }
+
+        else
+        {
+            capColl.enabled = true;
+            cirColl.enabled = false;
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Wall")
+    //    {
+    //        canMove = false;
+    //    }
+    //}
 }
