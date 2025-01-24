07e7fd81755523f9a0d586b4c9528eb5d7e73a1e
Nigoulash <g.n.oneill@outlook.com>
2025-01-24 09:30:52 +0100

    cutscene

--- Assets/Scripts/Cutscene/ChangeSceneOnTimer.cs
+++ Assets/Scripts/Cutscene/ChangeSceneOnTimer.cs
@@ -5,16 +5,17 @@ using UnityEngine.SceneManagement;
 public class ChangeSceneOnTimer : MonoBehaviour
 {
    
-   public float changeTime = 5f;
-   public string sceneName = "LinneJacobScene";
+   [SerializeField] float changeTime = 5f;
+   [SerializeField] string sceneName = "LinneJacobScene";
 
     // Update is called once per frame
     void Update()
     {
         changeTime -= Time.deltaTime;
-if(changeTime <- 0)
-{SceneManager.LoadScene(sceneName);
+        if(changeTime <- 0)
+        {
+            SceneManager.LoadScene(sceneName);
 
-}
+        }
     }
 }
